fc342157d2d782b4c748e7072e3811de1d8b92ea
KIPTOO-JOB <job3kiptoo@gmail.com>
2024-11-12 23:10:58 +0300

    mpesa Integration

--- app.py
+++ app.py
@@ -2,11 +2,16 @@
 from flask import Flask
 from extensions import db, migrate
 from Auth.auth import bp_auth
+import os
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    app.config["CALLBACK_URL"] = os.getenv("CALLBACK_URL")
+    app.config["CONSUMER_KEY"] = os.getenv("CONSUMER_KEY")
+    app.config["CONSUMER_SECRET"] = os.getenv("CONSUMER_SECRET")
 
     # Initialize extensions with app
     db.init_app(app)
@@ -14,6 +19,7 @@ def create_app():
 
     # Register blueprints
     app.register_blueprint(bp_auth, url_prefix='/auth')
+     # app.register_blueprint(bp_auth)
 
     return app
 
