c0aa68d3621b7c80968e1f26041cb10f5b83fd21
KIPTOO-JOB <job3kiptoo@gmail.com>
2024-11-18 12:49:24 +0300

    db

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 # app.py
 from flask import Flask
-from extensions import db, migrate
+
 from Auth.auth import bp_auth
+from extensions import db, migrate
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Initialize extensions with app
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register blueprints
-    app.register_blueprint(bp_auth, url_prefix='/auth')
+    app.register_blueprint(bp_auth, url_prefix="/auth")
 
     return app
+
 
 # Only for running locally
 if __name__ == "__main__":
